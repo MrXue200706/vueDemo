33688abbdc39ed29dfc92d98caf409fa5f8073b6
fawn97 <945814622@qq.com>
2017-11-19 16:02:13 +0800

    使用mui做底部

--- src/component/common/index.js
+++ src/component/common/index.js
@@ -1,10 +1,12 @@
 //编写自定义vue组件库（插件）
 import HeaderComponent from './header.vue';
+import FooterComponent from './footer.vue';
 
 //vue要求提供install方法，将插件注入vue
 //调用component扩展功能，如果是过滤器是filter，指令是directive
 export default{
     install(Vue){
         Vue.component('app-header',HeaderComponent);
+        Vue.component('app-footer', FooterComponent);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import MintUi from 'mint-ui';
 import 'mint-ui/lib/style.css';//只引入样式
 import Common from '../component/common';
+import 'mui/dist/css/mui.css';//引入样式
+import 'mui/examples/hello-mui/css/icons-extra.css';//引入字体图标
 
 //模块化后需要手动启用插件
 Vue.use(MintUi);
